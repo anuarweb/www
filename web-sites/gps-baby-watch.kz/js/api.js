ab29c8cd8d75ffc65b8c38ec204d3846e2b41ea7
Anu <anuarwebkz@gmail.com>
2017-03-08 21:58:04 +0600

    api yandex

--- web-sites/gps-baby-watch.kz/js/api.js
+++ web-sites/gps-baby-watch.kz/js/api.js
@@ -1,8 +1,21 @@
-ymaps.ready{init};
-    function init (){
-	var myMap = new ymaps.Map('map',{
-	center: [51.128422, 71.430564],
-	zoom: 11,
-	controls: []
-	})
+ymaps.ready(init);
+function init () {
+    var myMap = new ymaps.Map('map',{
+        center: [51.12, 71.43],
+        zoom: 12,
+        controls: ['zoomControl']
+    }),  
+    myGeoObject = new ymaps.GeoObject({        
+    });
+    myMap.geoObjects
+    .add(myGeoObject)
+    .add(new ymaps.Placemark([51.155877, 71.465089], {
+        balloonContentHeader: "ДС Казахстан",
+        balloonContentBody: "ДС Казахстан good",
+        balloonContentFooter: "offical site kaz.kz",
+        hintContent: "school adasd"
+    }, {
+        preset: "islands#sportIcon",
+        iconColor: "red",        
+    }));
 }
